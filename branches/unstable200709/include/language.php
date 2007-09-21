e861f0ee995d11750eb00955ac0d6c6f9737a20c
linuxppc <linuxppc@a648c742-521e-0410-8033-893827b2606d>
2007-09-21 05:38:24 +0000

    redoing the branch cause it didtn work all nice

---
+++ branches/unstable200709/include/language.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * Read language informations
+ * 1. reads default-language file
+ * 2. reads requested language file
+ * 3. make some editing (Upper-Case etc.)
+ * 
+ * Not in each translated file need to be each all translations, only in the default-lang-file (english)
+ */
+
+
+//http_negotiate_language($langs, $result);
+//print_r($result);
+unset($LANG);
+
+$language = getDefaultLanguage();
+
+function getLanguageArray() {
+	global $language;
+	$langPath = "./lang/";
+	$langFile = "/lang.php";
+	//$getLanguage = getenv("HTTP_ACCEPT_LANGUAGE");
+	//$language = getDefaultLanguage();
+
+	//include english as default - so if the selected lang doesnt have the required lang then it still loads
+	include($langPath."en-gb".$langFile);
+
+	include($langPath.$language.$langFile);
+
+	/*
+	if(file_exists($langPath.substr($getLanguage,0,2).$langFile)) {
+		include($langPath.substr($getLanguage,0,2).$langFile);
+	}
+	
+	if(file_exists($langPath.substr($getLanguage,0,5).$langFile)) {
+		include($langPath.substr($getLanguage,0,5).$langFile);
+	}
+	*/
+	
+	return $LANG;
+}
+
+function getLanguageList() {
+	$xmlFile = "info.xml";
+	$langPath = "lang/";
+	$folders = null;
+	
+	if($handle = opendir($langPath)) {
+		
+		//TODO: catch ., .. and other bad folders
+		for($i=0;$file = readdir($handle);$i++) {
+			$folders[$i] = $file;
+		}
+		closedir($handle);
+	}
+	
+	$languages = null;
+	$i = 0;
+	
+	foreach($folders as $folder) {
+		$file = $langPath.$folder."/".$xmlFile;
+		if(file_exists($file)) {
+			//echo $file."<br />";
+			$values = simplexml_load_file($file);
+			$languages[$i] = $values;
+			$i++;
+			//print_r($values);
+			//echo $values->name;
+		}
+	}
+	
+	return $languages;
+}
+
+$LANG = getLanguageArray();
+//TODO: if (getenv("HTTP_ACCEPT_LANGUAGE") != available language) AND (config lang != en) ) {
+// then use config lang
+// }
+//
+?>
---
+++ branches/unstable200709/modules/invoices/email.php
@@ -0,0 +1,111 @@
+<?php
+/*
+* Script: email.php
+* 	Email invoice page
+*
+* Authors:
+*	 Justin Kelly, Nicolas Ruflin
+*
+* Last edited:
+* 	 2007-07-19
+*
+* License:
+*	 GPL v2 or above
+*
+* Website:
+* 	http://www.simpleinvoices.org
+ */
+
+//stop the direct browsing to this file - let index.php handle which files get displayed
+checkLogin();
+
+#get the invoice id
+$invoice_id = $_GET['invoice'];
+
+$invoice = getInvoice($invoice_id);
+$preference = getPreference($invoice['preference_id']);
+$biller = getBiller($invoice['biller_id']);
+$customer = getCustomer($invoice['customer_id']);
+
+$sql = "SELECT inv_ty_description AS type FROM ".TB_PREFIX."invoice_type WHERE inv_ty_id = $invoice[type_id]";
+$query = mysqlQuery($sql);
+$invoiceType = mysql_fetch_array($query);
+
+$url_pdf = "http://{$http_auth}$_SERVER[HTTP_HOST]{$httpPort}$install_path/index.php?module=invoices&view=templates/template&invoice=$invoice_id&action=view&location=pdf&style=$invoiceType[type]";
+//echo $url_pdf;
+$url_pdf_encoded = urlencode($url_pdf); 
+$url_for_pdf = "http://{$http_auth}$_SERVER[HTTP_HOST]{$httpPort}$install_path/include/pdf/html2ps.php?process_mode=single&renderfields=1&renderlinks=1&renderimages=1&scalepoints=1&pixels=$pdf_screen_size&media=$pdf_paper_size&leftmargin=$pdf_left_margin&rightmargin=$pdf_right_margin&topmargin=$pdf_top_margin&bottommargin=$pdf_bottom_margin&transparency_workaround=1&imagequality_workaround=1&output=2&location=pdf&pdfname=$preference[pref_inv_wording]$invoice[id]&URL=$url_pdf_encoded";
+
+if ($_GET['stage'] == 2 ) {
+	if (extension_loaded('curl')) {
+		$ch = curl_init();
+	
+		// set URL and other appropriate options
+		curl_setopt($ch, CURLOPT_URL, $url_for_pdf);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		
+		// grab URL and pass it to the browser
+		$response = curl_exec($ch);
+		
+		// close cURL resource, and free up system resources
+		curl_close($ch);
+	}else{
+		$response = file_get_contents("http://www.example.com/", "r");
+	}
+	
+	//use curl and baring that use fopen
+	//
+	echo $block_stage2;
+
+	require("./modules/include/mail/class.phpmailer.php");
+
+	$mail = new PHPMailer();
+
+	$mail->IsSMTP();                                      // set mailer to use SMTP
+	$mail->Host = $email_host;  // specify main and backup server - separating with ;
+	$mail->SMTPAuth = $email_smtp_auth;     // turn on SMTP authentication
+	$mail->Username = $email_username;  // SMTP username
+	$mail->Password = $email_password; // SMTP password
+
+	$mail->From = "$_POST[email_from]";
+	$mail->FromName = "$biller[name]";
+	$mail->AddAddress("$_POST[email_to]");
+	if ($_POST[email_bcc]) {
+	$mail->AddBCC("$_POST[email_bcc]");
+	}
+	$mail->WordWrap = 50;                                 // set word wrap to 50 characters
+	$mail->AddAttachment("./include/pdf/out/$preference[pref_inv_wording]$invoice[id].pdf");         // add attachments
+
+	$mail->IsHTML(true);                                  // set email format to HTML
+
+	$mail->Subject = "$_POST[email_subject]"; 
+	$mail->Body    = "$_POST[email_notes]";
+	$mail->AltBody = "$_POST[email_notes]";
+
+	$results = $mail->Send();
+	unlink("./include/pdf/out/$preference[pref_inv_wording]$invoice[id].pdf");
+	if(!$results)
+	{
+	   echo "Message could not be sent. <p>";
+	   echo "Mailer Error: " . $mail->ErrorInfo;
+	   exit;
+	}
+	$message = "<META HTTP-EQUIV=REFRESH CONTENT=2;URL=index.php?module=invoices&view=manage><br>$preference[pref_inv_wording] $invoice[id] has been sent as a PDF";
+	echo $block_stage3;
+}
+
+//stage 3 = assemble email and send
+else if ($_GET['stage'] == 3 ) {
+	echo "How did you get here :)";
+}
+
+$pageActive = "invoices";
+
+$smarty -> assign('pageActive', $pageActive);
+$smarty -> assign('message', $message);
+$smarty -> assign('biller',$biller);
+$smarty -> assign('customer',$customer);
+$smarty -> assign('invoice',$invoice);
+$smarty -> assign('preferences',$preference);
+
+?>
