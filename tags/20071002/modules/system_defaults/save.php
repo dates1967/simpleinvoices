c0c5e6f3876c99ac5894dafece1ff67607167c85
linuxppc <linuxppc@a648c742-521e-0410-8033-893827b2606d>
2007-10-01 13:35:39 +0000

    tag the release

---
+++ tags/20071002/modules/system_defaults/save.php
@@ -0,0 +1,23 @@
+<?php
+
+checkLogin();
+
+# Deal with op and add some basic sanity checking
+
+$saved = false;
+
+
+error_log($_POST['name']."  ".$_POST['value']);
+
+//echo $_POST['value']."VAL";
+if (isset($_POST['op']) && $_POST['op'] == 'update_system_defaults' ) {
+	
+	if(updateDefault($_POST['name'],$_POST['value'])) {
+		$saved = true;
+	}
+}
+$pageActive = "options";
+$smarty->assign('pageActive', $pageActive);
+$smarty -> assign("saved",$saved);
+
+?>
